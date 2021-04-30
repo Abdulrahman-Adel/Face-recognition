435b7c648d32358936655ccde429b4498ea7b505
Abdulrahman-Adel <57441828+Abdulrahman-Adel@users.noreply.github.com>
2021-05-01 00:39:07 +0200

    Update

--- V2Fs.py
+++ V2Fs.py
@@ -7,10 +7,9 @@ Created on Wed Apr 28 00:15:12 2021
 
 import cv2
 import numpy as np
-from mtcnn.mtcnn import MTCNN
 import os
 
-def  V2Fs(Video_path, person_name, required_size = (160, 160)):
+def  V2Fs(Video_path,person_name, counter, required_size = (160, 160)):
     '''
     Parameters
     ---------- 
@@ -26,13 +25,14 @@ def  V2Fs(Video_path, person_name, required_size = (160, 160)):
     
     Returns
     -------
-    A dataset of extracted faces from each frame with it's respective label (persons' name).
+    None.
     '''
     
     cap = cv2.VideoCapture(Video_path)
-    detector = MTCNN()
     
-    i = 0
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    
+    i = counter
     
     X_train = []
     y_train = []
@@ -46,10 +46,10 @@ def  V2Fs(Video_path, person_name, required_size = (160, 160)):
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         pixels = np.asarray(image)
         
-        faces = detector.detect_faces(pixels)
+        faces = face_cascade.detectMultiScale(image, scaleFactor = 1.3, minNeighbors = 5)
         #To save only frames with one detectable face in them
         if(len(faces) == 1):
-            x, y, h, w = faces[0]['box']
+            x, y, h, w = faces[0]
             roi = pixels[y:y+h, x:x+w]    
             image = cv2.resize(roi, required_size)
             cv2.imwrite("images/"+person_name+"_"+str(i)+".jpg", image)
@@ -62,27 +62,29 @@ def  V2Fs(Video_path, person_name, required_size = (160, 160)):
     cap.release()
     cv2.destroyAllWindows()
     
-    return X_train, y_train
+    return X_train, y_train, i
 
 
     
 Base_dir = os.path.dirname(os.path.abspath(__file__))   
-
+Video_dir = os.path.join(Base_dir, "Videos")
 X, y = [], []
 
-for root, dirs, files in os.walk(Base_dir):
-    for file in files:
+for folder in os.listdir(Video_dir):
+    folder_path = os.path.join(Video_dir, folder)
+    counter = 0
+    for file in os.listdir(folder_path):
         if file.endswith("mp4"):
-           file_path = os.path.join(root, file)
-           trainX, trainy = V2Fs(file_path, file[:-4])
+            file_path = os.path.join(folder_path, file)
+            trainX, trainy, counter = V2Fs(file_path, folder, counter)
            
-           X.extend(trainX)
-           y.extend(trainy)
+            X.extend(trainX)
+            y.extend(trainy)
            
 
 #Saving the dataset
-np.savez_compressed('Face_authen.npz', X, y) 
-
+np.savez_compressed('Face_authen.npz', X, y)
+print("Dataset Saved !!")
         
            
 
